a35f5647565b6d54ab76d209a27cda3ca45a494e
Alex Bahouth <abahouth@onekingslane.com>
2014-09-17 11:28:47 -0700

    Got antlr to work\!

--- src/sqlgraph/core.clj
+++ src/sqlgraph/core.clj
@@ -1,5 +1,23 @@
 (ns sqlgraph.core
-  (:gen-class))
+  (:import [org.antlr.v4.runtime ANTLRFileStream]
+           [org.antlr.v4.runtime
+            ANTLRInputStream CommonTokenStream CharStream]
+           [org.antlr.v4.runtime.tree
+            ParseTree ParseTreeWalker]))
+
+(def sql-stmt "Select * from yourmom")
+
+
+
+(.ready sql-reader)
+(.close sql-reader)
+(let [foo (ANTLRInputStream. sql-stmt)]
+  (SQLLexer. foo))
+
+(instance? CharStream (ANTLRInputStream. sql-stmt) )
+
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
