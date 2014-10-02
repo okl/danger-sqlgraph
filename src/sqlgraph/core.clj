59a977a79a31877418fee60d0a6b83b38b6e79f1
Eric Sayle <esayle@onekingslane.com>
2014-10-02 14:40:36 -0700

    Better table name handling
    Tables are either produced or consumed and an object is returned with
    multiple tables in each state.

--- src/sqlgraph/core.clj
+++ src/sqlgraph/core.clj
@@ -13,16 +13,31 @@
             SQLParser$SqlContext
             SQLParserBaseListener]))
 
-(def sql-stmt "SELECT * FROM YOURMOM;")
+(def sql-stmt "CREATE TABLE IQ.TEST AS SELECT * FROM SCHEMA.YOURMOM Q LEFT JOIN SCHEMA.MYMOM T ON Q.FY = T.FY;")
 
-(def words (atom ""))
+(def results (atom {:produces [] :consumes []}))
+(def state (atom nil))
+
+(defn add-table [table-name]
+  (let [mode (if (= "query" (first @state)) :consumes :produces)]
+    (swap! results #(assoc % mode (conj (mode %) table-name)))))
 
 (defn make-listener []
   (proxy [okl.sqlgraph.SQLParserBaseListener] []
+    (enterCreate_table_statement [^SQLParser$SqlContext ctx]
+      (swap! state #(conj % "create"))
+      (println state))
+    (exitCreate_table_statement [^SQLParser$SqlContext ctx]
+      (swap! state #(rest %))
+      (println state))
+    (enterQuery_expression [^SQLParser$SqlContext ctx]
+      (swap! state #(conj % "query"))
+      (println state))
+    (exitQuery_expression [^SQLParser$SqlContext ctx]
+      (swap! state #(rest %))
+      (println state))
     (enterTable_name [^SQLParser$SqlContext ctx]
-      (swap! words #(str % "<TNAME>" (.getText ctx))))
-    (exitTable_name [^SQLParser$SqlContext ctx]
-      (swap! words #(str % "</TNAME>")))))
+      (add-table (.getText ctx)))))
 
 (defn parse-expr [s]
   (let [lexer (SQLLexer. (ANTLRInputStream. s))
