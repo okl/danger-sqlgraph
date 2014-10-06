222b4771dc310a61269fc6c1873ba6c49605a7aa
Eric Sayle <esayle@onekingslane.com>
2014-10-06 14:29:39 -0700

    Handling multiple statements per file

--- src/sqlgraph/cli.clj
+++ src/sqlgraph/cli.clj
@@ -19,6 +19,13 @@
 (defn- error-msg [errors summary]
   (string/join "\n" (concat errors [(usage summary)])))
 
+(defn- process-stmt [stmt]
+  (println (parse-expr (str (string/trim stmt) ";"))))
+
+(defn- process-file [file-contents]
+  "Process each statement of the file separately"
+  (doall (map process-stmt (string/split file-contents #";"))))
+
 (defn -main [& args]
   (let [opts-out (parse-opts args cli-opts)]
     (if (:errors opts-out)
@@ -26,5 +33,5 @@
         (println (error-msg (:errors opts-out) (:summary opts-out)))
         (System/exit 1)))
     (if (:file (:options opts-out))
-      (println (parse-expr (slurp (:file (:options opts-out)))))
+      (process-file (slurp (:file (:options opts-out))))
       (println (parse-expr (string/join "\n" (:arguments opts-out)))))))
